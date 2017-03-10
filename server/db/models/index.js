67f859bf560c9bc2587604f61ac3a7bae023d846
Brandon Culp <bculp513@gmail.com>
2017-03-09 20:42:46 -0500

    updated index file in models so would have example for future

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,1 +1,15 @@
 // Require models and form associations
+
+const db = require('./db');
+
+// Require our models. Running each module registers the model into sequelize
+// so any other part of the application can simply call sequelize.model('User')
+// to get access to the User model.
+require('./models');
+
+// Syncing all the models at once. This promise is used by main.js.
+const syncedDbPromise = db.sync();
+
+syncedDbPromise.then(() => console.log('Sequelize models synced to PostgreSQL'));
+
+module.exports = syncedDbPromise;
